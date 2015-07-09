f34a13331efa99cf4791312f6132c85209427e7d
Nikita Barsukov <nikita@barsukov.net>
2015-07-10 00:18:45 +0200

    Methods for drawing running routes are ready

---
+++ public/running_route.js
@@ -0,0 +1,35 @@
+
+function draw_map(d, t, pan){
+    c = turf.centroid(d['geojson'])['geometry']['coordinates'];
+    if(pan){
+        map.panTo(new L.LatLng(c[1], c[0]))
+    }
+    function transition(p) {
+        p.transition()
+            .duration(t)
+            .attrTween("stroke-dasharray", tweenDash);
+    }
+
+    function tweenDash() {
+        var l = feature.node().getTotalLength();
+        var i = d3.interpolateString("0," + l, l + "," + l);
+        return function(t) {
+            return i(t)
+        }
+    }
+
+    var feature = g.append("path")
+        .datum(d['geojson'])
+        .attr({
+            "id": "path_" + d['id'],
+            "d":path
+        })
+        .call(transition);
+
+    map.on("viewreset", function() {
+        feature.attr({
+            "d":path,
+            "stroke-dasharray": ''
+        })
+    })
+}
